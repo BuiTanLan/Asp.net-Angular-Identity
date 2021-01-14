1b5751da97cf43562be01ea4f5e80374e1442483
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2021-01-14 17:29:00 +0700

    half role base

--- AngularClient/src/app/app.module.ts
+++ AngularClient/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 import { ErrorHandlerService } from './shared/services/error-handler.service';
+import { JwtModule } from '@auth0/angular-jwt';
+
+
 
 @NgModule({
   declarations: [
@@ -25,7 +28,14 @@ import { ErrorHandlerService } from './shared/services/error-handler.service';
     { path: '404', component: NotFoundComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/404', pathMatch: 'full' }
-], { relativeLinkResolution: 'legacy' })
+    ], { relativeLinkResolution: 'legacy' }),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => localStorage.getItem('token'),
+        allowedDomains:  ['localhost:5001'],
+        disallowedRoutes: []
+      }
+    })
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorHandlerService, multi: true },
